2487cbb48cba33b1460303565c6a18c857e59872
Jay Prakash S <jpcricketer1998@gmail.com>
2023-01-14 08:40:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// var userInput = document.querySelector("#user-input");
+// var txtLoaded = document.querySelector("#loaded-txt")
+// txtLoaded.addEventListener("click", ()=>
+// {
+//   userInput.style.display= "none";
+// })
+
+var loading= document.querySelector('.loading');
+var btnLoaded= document.querySelector('.btn-loaded');
+
+btnLoaded.addEventListener('click',()=>{
+    loading.style.display="none"
+})
